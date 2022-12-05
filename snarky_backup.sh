cda6931eddf67b6e9c8071d5b54baee3f58235aa
Aidan Loughran <aidlo.dev@pm.me>
2022-12-05 19:10:16 +0000

    Only check the dest drive
    **Me:** Can we change the check for the drive being full to only check the drive of the destination path that the user gave
    
    **ChatGPT:** Sure, here's a modified version of the script that only checks the drive of the destination path that the user provided:
    
    In this version, the script uses the `df` command to get the drive of the destination directory, and then checks if that drive is 100% full. If it is, the script prints a snarky message and exits with an error code. This ensures that the script only checks the drive of the destination directory provided by the user, rather than checking all the drives on the system.

--- snarky_backup.sh
+++ snarky_backup.sh
@@ -11,8 +11,11 @@ if [ -z "$source" ] || [ -z "$destination" ]; then
   exit 1
 fi
 
+# Get the drive of the destination directory
+destination_drive=$(df -P "$destination" | tail -1 | awk '{print $1}')
+
 # Check if the drive is full
-if df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | grep -E '^100%' >/dev/null 2>&1; then
+if df -H "$destination_drive" | grep -E '^100%' >/dev/null 2>&1; then
   # Tell you off for being a data hoarder
   echo "Hey, you're a data hoarder! You need to clean up your drive and make some space before I can run the backup."
   exit 1
